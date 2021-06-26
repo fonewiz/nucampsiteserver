bd9f04c856bd030cf342ca221e5553818e9e8271
Todd Routhier <fonemasta@gmail.com>
2021-06-26 11:52:17 -0500

    Week 2 Assignment

---
+++ models/partner.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Partner = mongoose.model('Partner', partnerSchema);
+
+module.exports = Partner;
---
+++ models/promotion.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    description: {
+        type: String,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const Promotion = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotion;
